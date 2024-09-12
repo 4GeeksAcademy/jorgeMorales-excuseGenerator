0c0a14c7c28532af55488ec3301303d70e8e10f7
Jm9710 <85179035+Jm9710@users.noreply.github.com>
2024-09-12 00:47:50 +0000

    generador de excusa terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let excusa =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+
+    document.getElementById("excuse").innerHTML = excusa;
+  }
+
+  document
+    .getElementById("new-excuse-btn")
+    .addEventListener("click", function(e) {
+      e.preventDefault();
+      generateExcuse();
+    });
+
   console.log("Hello Rigo from the console!");
 };
